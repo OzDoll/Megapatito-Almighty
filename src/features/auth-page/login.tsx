d0c9b24379543b5b63b06b6ac15421e06139d3ff
Damian Márquez <einsam.lumiere@gmail.com>
2024-04-05 05:31:34 +0200

    Center Login

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -1,16 +1,9 @@
-"use client";
 import { AI_NAME } from "@/features/theme/theme-config";
 import { signIn } from "next-auth/react";
 import { FC } from "react";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 interface LoginProps {
   isDevMode: boolean;
@@ -18,26 +11,28 @@ interface LoginProps {
 
 export const LogIn: FC<LoginProps> = (props) => {
   return (
-    <Card className="flex gap-2 flex-col min-w-[300px]">
-      <CardHeader className="gap-2">
-        <CardTitle className="text-2xl flex gap-2">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src={"ai-icon.png"} />
-          </Avatar>
-          <span className="text-primary">{AI_NAME}</span>
-        </CardTitle>
-        <CardDescription>
-          Login in with your Microsoft account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("azure-ad")}> Microsoft </Button>
-        {props.isDevMode ? (
-          <Button onClick={() => signIn("localdev")}>
-            Basic Auth (DEV ONLY)
-          </Button>
-        ) : null}
-      </CardContent>
-    </Card>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Card className="flex gap-2 flex-col min-w-[300px]">
+        <CardHeader className="gap-2">
+          <CardTitle className="text-2xl flex gap-2">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src={"ai-icon.png"} />
+            </Avatar>
+            <span className="text-primary">{AI_NAME}</span>
+          </CardTitle>
+          <CardDescription>
+            Login in with your Microsoft Account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <Button onClick={() => signIn("azure-ad")}> Log in </Button>
+          {props.isDevMode ? (
+            <Button onClick={() => signIn("localdev")}>
+              Basic Auth (DEV ONLY)
+            </Button>
+          ) : null}
+        </CardContent>
+      </Card>
+    </div>
   );
 };
--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,8 @@
 export const AI_NAME = "Megapatito Allmighty";
-export const AI_DESCRIPTION = "Megapatito FinOps Copilot is a project by Damian Marquez and Manuel Beck, powered by Azure AI";
+export const AI_DESCRIPTION = "Megapatito FinOps Copilot is a project by Damian Marquez and Manuel Beck, powered by Azure Open AI.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + "Megapatito";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are ${AI_NAME}, an Azure AI technology assistant designed for Azure Cloud Architects. Your role is to help you with any questions or issues you may have related to technology, Microsoft and Azure. Whether the person needs help with programming, troubleshooting technical problems, or just want to stay up-to-date with the latest trends and developments in the industry, You're here to assist. And you'll always do your best to provide accurate and helpful information. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
