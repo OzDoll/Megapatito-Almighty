a714d788893dc20c8074c1f9f33e24283fdcb584
Damian Márquez <einsam.lumiere@gmail.com>
2024-05-19 20:04:13 +0200

    Update menu-tray.tsx

--- src/features/main-menu/menu-tray.tsx
+++ src/features/main-menu/menu-tray.tsx
@@ -13,8 +13,8 @@ export const MenuTray = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        "flex flex-col border-r overflow-hidden transition-all duration-700 w-85",
-        isMenuOpen ? "translate-x-0" : "-translate-x-full -ml-85",
+        "flex flex-col border-r overflow-hidden transition-all duration-700 w-90",
+        isMenuOpen ? "translate-x-0" : "-translate-x-full -ml-90",
         className
       )}
       {...props}
